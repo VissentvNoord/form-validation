d85d08ec365aaa365f615e9d641c89782a809bbd
VissentvNoord <49689172+VissentvNoord@users.noreply.github.com>
2023-11-01 16:05:27 +0100

    Form complete

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const username = document.getElementById('username');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+const zipcode = document.getElementById('zip-code');
+
+const validIndicator = document.getElementById('valid-indicator');
+
+let valid = true;
 
 form.addEventListener('submit', (e) => {
      e.preventDefault();
@@ -8,9 +15,13 @@ form.addEventListener('submit', (e) => {
      checkInputs();
 })  
 
+
 function checkInputs(){
     const emailVal = email.value.trim();
     const usernameVal = username.value.trim();
+    const passwordVal = password.value.trim();
+    const confirmPasswordVal = confirmPassword.value.trim();
+    const zipcodeVal = zipcode.value.trim();
 
     if(usernameVal === ''){
         //error
@@ -24,6 +35,46 @@ function checkInputs(){
     }else{
         setValidFor(email);
     }
+
+    if(passwordVal === ''){
+        setInvalidFor(password, 'Password cannot be blank');
+
+        if(confirmPasswordVal === ''){
+            setInvalidFor(confirmPassword, 'Password cannot be blank');
+        }
+    }else{
+        setValidFor(password);
+
+        if(confirmPasswordVal === ''){
+            setInvalidFor(confirmPassword, 'Password cannot be blank');
+        }else if(confirmPasswordVal != passwordVal){
+            setInvalidFor(confirmPassword, 'Passwords do not match');
+        }else{
+            setValidFor(confirmPassword);
+        }
+    }
+
+    if(zipcodeVal === ''){
+        setInvalidFor(zipcode, 'Zip-code cannot be blank');
+    }else{
+        if(validZipCode(zipcodeVal)){
+            setValidFor(zipcode);
+        }else{
+            setInvalidFor(zipcode, 'Incorrect format!')
+        }
+    }
+
+    validOrNot();
+}
+
+function validOrNot(){
+    if(valid){
+        validIndicator.className = 'visible';
+    }
+}
+
+function validZipCode(zipcode){
+    return /^\d{5}(-\d{4})?$/.test(zipcode);
 }
 
 function setInvalidFor(element, message){
@@ -33,6 +84,8 @@ function setInvalidFor(element, message){
     small.classList.remove('hidden');
     small.classList.add('visible');
     formElement.className = 'form-element invalid';
+
+    valid = false;
 }
 
 function setValidFor(element){
